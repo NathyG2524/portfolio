6970f59c11230a63b4fdaa482a0a5771e5c3479d
NathyG2524 <nathygirma2524@gmail.com>
2022-09-22 11:35:42 +0300

    feat: change mock data

--- src/data.jsx
+++ src/data.jsx
@@ -1,40 +1,41 @@
-import Pro1 from "./assets/project-1.jpg"
+import Pro1 from "./assets/project1.png"
 import Pro2 from "./assets/project-2.jpg"
 import Pro3 from "./assets/project-3.jpg"
 import Pro4 from "./assets/project-4.jpg"
 
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
+      title: "Buildit",
+      subtitle: "Django and React",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+"Buildit is a single webpage builder. you can select a templates to work with or start from scretch on the editor",
       image: Pro1,
-      link: "https://reactbootcamp.com",
+      link: "https://letsbuidit.netlify.app/",
+      github: "https://github.com/NathyG2524/Buildit",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "skillz",
+      subtitle: "Django",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Skillz is an e-learning platform where everyone can upload a course and learn from other upload courses",
       image: Pro2,
-      link: "https://reedbarger.com",
+      github: "https://github.com/NathyG2524/Skillz-Elearning",
     },
     {
-      title: "DevChat",
-      subtitle: "React and Firebase",
+      title: "Coffee Shop",
+      subtitle: "Flask backend",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Udacity has decided to open a new digitally enabled cafe for students to order drinks, socialize, and study hard. But they need help setting up their menu experience",
       image: Pro3,
-      link: "https://jsbootcamp.com",
+      github: "https://github.com/NathyG2524/coffe_shop",
     },
     {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
+      title: "Trivia App",
+      subtitle: "Flask backend",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Udacity is invested in creating bonding experiences for its employees and students. A bunch of team members got the idea to hold trivia on a regular basis and created a webpage to manage the trivia app and play the game, but their API experience is limited and still needs to be built out.",
       image: Pro4,
-      link: "https://pythonbootcamp.com",
+      github: "https://github.com/NathyG2524/Trivia",
     },
   ];
   export const testimonials = [
@@ -44,7 +45,7 @@ export const projects = [
     //   quote:
     //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     //   image: Pro1,
-    //   link: "https://reactbootcamp.com",
+    //   github: "https://reactbootcamp.com",
     // },
     // {
     //   name: "React Tracks",
